50818cf55cf4763ee95acdecf5a5b533bc16e313
iiesteban <e.ortiz06@ufromail.cl>
2025-05-12 01:51:18 -0400

    chore(core): create app_colors with initial input border color

---
+++ frontend/lib/core/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF5988FF);
+  
+  static const Color inputBorder = Color.fromARGB(150, 75, 75, 75);
+}
