b9deca891f5c2215a5f1a09729f91de95eeb1e5a
iiesteban <e.ortiz06@ufromail.cl>
2025-05-25 23:38:08 -0400

    feat(api): add ApiClient class for handling HTTP requests with ResponseDTO

---
+++ frontend/lib/common/network/api_client.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+import 'package:frontend/common/dto/response.dart';
+import 'package:http/http.dart' as http;
+
+class ApiClient {
+  final String baseUrl = 'http://localhost:8080/api';
+
+  Future<ResponseDTO> post(
+    String endpoint, {
+    Map<String, String>? headers,
+    Map<String, String>? queryParams,
+    Object? body,
+  }) async {
+    try {
+      final response = await http.post(
+        Uri.parse(baseUrl + endpoint).replace(queryParameters: queryParams),
+        headers: headers ?? {'Content-Type': 'application/json'},
+        body: jsonEncode(body),
+      );
+
+      final jsonResponse = jsonDecode(response.body);
+
+      return ResponseDTO.fromJson(jsonResponse);
+    } catch (e) {
+      return ResponseDTO(
+        success: false,
+        message: 'Error de red: ${e.toString()}',
+        data: null,
+      );
+    }
+  }
+
+  Future<ResponseDTO> get(
+  String endpoint, {
+  Map<String, String>? headers,
+  Map<String, String>? queryParams, 
+}) async {
+  try {
+    final uri = Uri.parse(baseUrl + endpoint).replace(queryParameters: queryParams);
+    final response = await http.get(
+      uri,
+      headers: headers ?? {'Content-Type': 'application/json'},
+    );
+
+    final jsonResponse = jsonDecode(response.body);
+
+    return ResponseDTO.fromJson(jsonResponse);
+  } catch (e) {
+    return ResponseDTO(
+      success: false,
+      message: 'Error de red: ${e.toString()}',
+      data: null,
+    );
+  }
+}
+}
