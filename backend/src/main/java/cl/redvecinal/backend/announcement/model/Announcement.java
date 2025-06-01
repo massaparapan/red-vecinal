fa0763cd8bb3890525532a6849a0eff84e896b8d
iiesteban <e.ortiz06@ufromail.cl>
2025-05-31 21:10:04 -0400

    feat(announcement): add Announcement, AnnouncementLike, and AnnouncementType models

---
+++ backend/src/main/java/cl/redvecinal/backend/announcement/model/Announcement.java
@@ -0,0 +1,30 @@
+package cl.redvecinal.backend.announcement.model;
+
+import cl.redvecinal.backend.community.model.Community;
+import cl.redvecinal.backend.user.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Announcement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    @ManyToOne
+    private User createdBy;
+    @ManyToOne
+    private Community community;
+    @OneToMany(mappedBy = "announcement", cascade = CascadeType.ALL)
+    private Set<AnnouncementLike> likes;
+}
---
+++ backend/src/main/java/cl/redvecinal/backend/announcement/model/AnnouncementType.java
@@ -0,0 +1,7 @@
+package cl.redvecinal.backend.announcement.model;
+
+public enum AnnouncementType {
+    EMERGENCY,
+    ANNOUNCEMENT,
+    MEETING,
+}
