5cd82137e8a27ac34fd07b06a4c780ada86358e7
iiesteban <e.ortiz06@ufromail.cl>
2025-06-03 12:00:48 -0400

    feat(user): refactor User entity and related classes removing Swagger annotations

--- backend/src/main/java/cl/redvecinal/backend/user/dto/UserDto.java
+++ backend/src/main/java/cl/redvecinal/backend/user/dto/UserDto.java
@@ -1,16 +1,11 @@
 package cl.redvecinal.backend.user.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-@Schema(description = "Data Transfer Object for User information.")
+
 @Data
 public class UserDto {
-    @Schema(description = "Unique identifier for the user.")
     private Long id;
-    @Schema(description = "Username of the user.")
     private String username;
-    @Schema(description = "Phone number of the user.")
     private String phone;
-    @Schema(description = "Password of the user. This field should be stored securely.")
     private String password;
 }
--- backend/src/main/java/cl/redvecinal/backend/user/service/UserService.java
+++ backend/src/main/java/cl/redvecinal/backend/user/service/UserService.java
@@ -1,8 +1,5 @@
 package cl.redvecinal.backend.user.service;
 
-import cl.redvecinal.backend.user.dto.UserDto;
-
 public interface UserService {
-    UserDto getUserById();
     boolean isUserRegistered(String phoneNumber);
 }
