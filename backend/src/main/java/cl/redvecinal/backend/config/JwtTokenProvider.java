30ef79b9dd67500690147eac86ee08ed8965326e
iiesteban <e.ortiz06@ufromail.cl>
2025-06-03 20:52:17 -0400

    feat(jwt): fix phone number key typo and add password reset token generation

--- backend/src/main/java/cl/redvecinal/backend/config/JwtTokenProvider.java
+++ backend/src/main/java/cl/redvecinal/backend/config/JwtTokenProvider.java
@@ -1,20 +1,17 @@
 package cl.redvecinal.backend.config;
 
+import cl.redvecinal.backend.config.exception.TokenInvalidException;
 import cl.redvecinal.backend.user.model.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
-import java.util.HashMap;
 import io.jsonwebtoken.io.Decoders;
-import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.function.Function;
 @Component
 public class JwtTokenProvider {
@@ -26,7 +23,7 @@ public class JwtTokenProvider {
         Date now = new Date();
         return Jwts.builder()
                 .claims()
-                .add("phonNumber", user.getPhoneNumber())
+                .add("phoneNumber", user.getPhoneNumber())
                 .subject(String.valueOf(user.getId()))
                 .issuedAt(now)
                 .expiration(new Date(now.getTime() + jwtExpiration))
@@ -34,12 +31,23 @@ public class JwtTokenProvider {
                 .signWith(getKey())
                 .compact();
     }
+
+    public String generatePasswordResetToken(String phoneNumber) {
+        Date now = new Date();
+        return Jwts.builder()
+                .claim("phoneNumber", phoneNumber)
+                .issuedAt(now)
+                .expiration(new Date(now.getTime() + 600000))
+                .signWith(getKey())
+                .compact();
+    }
+
     private SecretKey getKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
     public String extractPhoneNumber (String token) {
-        return extractClaim(token, claims -> claims.get("phonNumber", String.class));
+        return extractClaim(token, claims -> claims.get("phoneNumber", String.class));
     }
     public Long extractUserId(String token) {
         return Long.parseLong(extractClaim(token, Claims::getSubject));
@@ -60,7 +68,7 @@ public class JwtTokenProvider {
             extractAllClaims(token);
             return true;
         } catch (JwtException | IllegalArgumentException e) {
-            throw new NoSuchElementException("Token no valido");
+            throw new TokenInvalidException("Token no valido");
         }
     }
     private boolean isTokenExpired(String token) {
