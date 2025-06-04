8c3c5cc3c1fd71bbdb69a66032729d8877adb837
iiesteban <e.ortiz06@ufromail.cl>
2025-06-03 20:51:06 -0400

    feat(otp): enhance OTP verification to include password reset token generation

--- backend/src/main/java/cl/redvecinal/backend/otp/controller/OtpController.java
+++ backend/src/main/java/cl/redvecinal/backend/otp/controller/OtpController.java
@@ -2,12 +2,15 @@ package cl.redvecinal.backend.otp.controller;
 
 import cl.redvecinal.backend.common.dto.SuccesResponse;
 import cl.redvecinal.backend.common.util.ResponseHelper;
+import cl.redvecinal.backend.config.JwtTokenProvider;
 import cl.redvecinal.backend.otp.service.OtpService;
 import cl.redvecinal.backend.otp.dto.OtpVerifyRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RequestMapping("/api/otp")
@@ -15,16 +18,20 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class OtpController {
     private final OtpService otpService;
+    private final JwtTokenProvider jwtTokenProvider;
     @PostMapping("/send")
     public ResponseEntity<SuccesResponse> sendCode (@RequestParam String phoneNumber) {
         otpService.sendOTP(phoneNumber);
         return ResponseHelper.success("OTP enviado correctamente");
     }
     @PostMapping("/verify")
-    public ResponseEntity<SuccesResponse> verifyCode (@RequestBody OtpVerifyRequest request) {
-        return ResponseHelper.success(Map.of(
-                "valid", otpService.verifyOTP(request.getPhoneNumber(), request.getCode()
-                )
-        ));
+    public ResponseEntity<SuccesResponse> verifyCode (@RequestBody @Valid OtpVerifyRequest request) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("valid", otpService.verifyOTP(request.getPhoneNumber(), request.getCode()));
+        if (response.get("valid").equals(Boolean.TRUE)) {
+            String token = jwtTokenProvider.generatePasswordResetToken(request.getPhoneNumber());
+            response.put("token", token);
+        }
+        return ResponseHelper.success(response);
     }
 }
