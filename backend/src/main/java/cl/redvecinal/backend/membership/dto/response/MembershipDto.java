0bcb75dce74ad984ea6a132dc92841595cf6d327
iiesteban <e.ortiz06@ufromail.cl>
2025-06-05 03:04:27 -0400

    refactor(membership): remove default values from MembershipDto and MembershipRequestDto

--- backend/src/main/java/cl/redvecinal/backend/membership/dto/response/MembershipDto.java
+++ backend/src/main/java/cl/redvecinal/backend/membership/dto/response/MembershipDto.java
@@ -1,12 +1,11 @@
 package cl.redvecinal.backend.membership.dto.response;
 
-import cl.redvecinal.backend.community.dto.CommunityDto;
 import cl.redvecinal.backend.membership.model.enums.MembershipRole;
 import cl.redvecinal.backend.membership.model.enums.MembershipStatus;
 import lombok.Data;
 
 @Data
 public class MembershipDto {
-    private MembershipStatus status = MembershipStatus.PENDING;
-    private MembershipRole role = MembershipRole.MEMBER;
+    private MembershipStatus status;
+    private MembershipRole role;
 }
--- backend/src/main/java/cl/redvecinal/backend/membership/dto/response/MembershipRequestDto.java
+++ backend/src/main/java/cl/redvecinal/backend/membership/dto/response/MembershipRequestDto.java
@@ -2,13 +2,13 @@ package cl.redvecinal.backend.membership.dto.response;
 
 import cl.redvecinal.backend.membership.model.enums.MembershipRole;
 import cl.redvecinal.backend.membership.model.enums.MembershipStatus;
-import cl.redvecinal.backend.user.dto.UserDto;
+import cl.redvecinal.backend.user.dto.request.UserDto;
 import lombok.Data;
 
 @Data
 public class MembershipRequestDto {
     private Long id;
-    private MembershipStatus status = MembershipStatus.PENDING;
-    private MembershipRole role = MembershipRole.MEMBER;
+    private MembershipStatus status;
+    private MembershipRole role;
     private UserDto userDto;
 }
