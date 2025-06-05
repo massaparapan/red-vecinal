214d3ffa50e3982649a05a3ead5aeb2b2bde52db
iiesteban <e.ortiz06@ufromail.cl>
2025-06-05 15:54:09 -0400

    feat(navigation): implement NavegationService for route management

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/common/service/navegation_service.dart';
 import 'package:frontend/screens/auth/auth_page.dart';
+import 'package:frontend/screens/menu_screen/admin_home.dart';
+import 'package:frontend/screens/menu_screen/no_community_home.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +19,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 255, 255)),
       ),
-      home: const AuthPage(),
+      navigatorKey: NavegationService().navigatorKey,
+      initialRoute: "/",
+      routes: {
+        '/': (context) => const AuthPage(),
+        '/home/admin': (context) => const AdminHomeScreen(), 
+        '/home/no-community': (context) => const NoCommunityScreen(), 
+      }
     );
   }
 }
