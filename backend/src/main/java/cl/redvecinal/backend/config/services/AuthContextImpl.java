59bf3c7dd30d0375a6004fd827491051cf9ba643
iiesteban <e.ortiz06@ufromail.cl>
2025-06-04 21:22:00 -0400

    refactor(auth): implement IAuthContext

---
+++ backend/src/main/java/cl/redvecinal/backend/config/services/AuthContextImpl.java
@@ -0,0 +1,22 @@
+package cl.redvecinal.backend.config.services;
+
+import cl.redvecinal.backend.config.CustomUserDetails;
+import cl.redvecinal.backend.user.model.User;
+import cl.redvecinal.backend.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthContextImpl implements IAuthContext {
+    private final UserRepository userRepository;
+    public User getCurrentUser() {
+        CustomUserDetails authentication = (CustomUserDetails) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+        return userRepository.findById(authentication.user().getId())
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado en el contexto de autenticación"));
+    }
+}
