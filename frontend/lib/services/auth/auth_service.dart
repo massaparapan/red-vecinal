20a3b67cd2c955ae63e3c9eca496a43a0b2a5a21
Martin Zuñiga <martinzunigacampos@gmail.com>
2025-06-04 23:41:02 -0400

    fix(auth): store token after login and use it in authenticated requests

--- frontend/lib/services/auth/auth_service.dart
+++ frontend/lib/services/auth/auth_service.dart
@@ -1,21 +1,42 @@
 import 'package:frontend/common/dto/response.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:frontend/common/network/api_client.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthService {
   final ApiClient _client = ApiClient();
   final String baseUrl = "/auth";
+  
+  final storage = FlutterSecureStorage();
+
+  Future<void> guardarToken(String token) async {
+    await storage.write(key: 'token', value: token);
+  }
+  
 
   Future<ResponseDTO> register(String phoneNumber, String username, String address, String password) async {
     final result = await _client.post(
       '$baseUrl/register',
+
       body: {'username': username,'phoneNumber': phoneNumber, 'password': password},
     );
+
+    if (result.success && result.data is Map<String, dynamic>) {
+      final data = result.data as Map<String, dynamic>;
+      final token = data['token'] as String?;
+
+      if (token != null) {
+        final storage = FlutterSecureStorage();
+        await storage.write(key: 'token', value: token);
+      }
+    }
 
     return result;
   }
 
   Future<ResponseDTO> login(String phoneNumber, String password) async {
+
+    final storage = FlutterSecureStorage();
     
     if (phoneNumber == "" || password == "") {
       return ResponseDTO(success: false, message: 'Por favor completar los campos');
@@ -26,11 +47,20 @@ class AuthService {
       body: {'phoneNumber': phoneNumber, 'password': password},
     );
 
+    print('Result success: ${result.success}');
+    print('Result data: ${result.data}');
+    print('Result message: ${result.message}');
+    if (result.success && result.data is String) {
+      final tokenRaw = result.data as String;
+
+      final token = tokenRaw.split("token: ").last.trim();
+
+      print('Token extraído: $token');
+      await storage.write(key: 'token', value: token);
+    }
+
     return result;
   }
 
-  Future<void> saveToken(String token) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('auth_token', token);
-  }
+
 }
--- frontend/lib/services/community_service.dart
+++ frontend/lib/services/community_service.dart
@@ -1,34 +1,40 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class CommunityService {
-  static const String baseUrl = 'http://10.0.2.2:8080/api/community';
+  static const String baseUrl = 'http://10.0.2.2:8080/api/communities';
 
   static Future<bool> createCommunity({
     required String name,
     required String description,
     required double lat,
     required double lon,
-    required int membersCount,
-    required String creationDate,
   }) async {
+
     final url = Uri.parse('$baseUrl/create');
     final body = jsonEncode({
       'name': name,
       'description': description,
       'lat': lat,
       'lon': lon,
-      'membersCount': membersCount,
-      'creationDate': creationDate,
     });
 
+    final storage = FlutterSecureStorage();
+    final token = await storage.read(key: 'token');
+
+    if (token == null) {
+      print('Error: No token found');
+      return false;
+    }
+    print('Error: ${token}');
     final response = await http.post(
       url,
-      headers: {'Content-Type': 'application/json'},
+      headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer $token'},
       body: body,
     );
 
-    if (response.statusCode == 201) {
+    if (response.statusCode == 200) {
       return true;
     } else {
       print('Error: ${response.body}');
@@ -41,12 +47,17 @@ class CommunityService {
   required double lon,
   }) async {
     final url = Uri.parse(
-      'http://10.0.2.2:8080/api/community/close?lat=$lat&lon=$lon',
+      'http://10.0.2.2:8080/api/communities/close?lat=$lat&lon=$lon',
     );
+
+    final storage = FlutterSecureStorage();
+    final token = await storage.read(key: 'token');
 
     print('📡 GET: $url');
 
-    final response = await http.get(url);
+    final response = await http.get(url,
+      headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer $token'},
+    );
 
     print('📥 STATUS: ${response.statusCode}');
     print('📥 BODY: ${response.body}');
@@ -54,6 +65,7 @@ class CommunityService {
     if (response.statusCode == 200) {
       final decoded = jsonDecode(response.body);
       print('📦 JSON decodificado: $decoded');
+
       return List<Map<String, dynamic>>.from(json.decode(response.body));
     } else {
       return [];
