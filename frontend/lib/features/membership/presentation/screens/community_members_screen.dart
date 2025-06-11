14a6e3a125ce9e20add69dcc47f6416fbc092e75
iiesteban <e.ortiz06@ufromail.cl>
2025-06-11 00:54:52 -0400

    feat(community): add CommunityMembersScreen with initial structure

---
+++ frontend/lib/features/membership/presentation/screens/community_members_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CommunityMembersScreen extends StatefulWidget {
+  const CommunityMembersScreen({super.key});
+
+  @override
+  State<CommunityMembersScreen> createState() => _CommunityMembersScreenState();
+}
+
+class _CommunityMembersScreenState extends State<CommunityMembersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
