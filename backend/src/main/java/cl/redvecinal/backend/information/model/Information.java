cd507e550af99965d47d21a4caf954b17eb4af8f
iiesteban <e.ortiz06@ufromail.cl>
2025-06-25 01:09:45 -0400

    feat(information): add constructors and id generation strategy to Information entity

--- backend/src/main/java/cl/redvecinal/backend/information/model/Information.java
+++ backend/src/main/java/cl/redvecinal/backend/information/model/Information.java
@@ -1,17 +1,20 @@
 package cl.redvecinal.backend.information.model;
 
 import cl.redvecinal.backend.community.model.Community;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Information {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
