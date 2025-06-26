22e48b2bdabe5a07fff6ffdcb2b97b0be631b9e4
iiesteban <e.ortiz06@ufromail.cl>
2025-06-26 17:06:33 -0400

    feat(event): implement EventCreateRequest and EventResponse models with JSON serialization

---
+++ frontend/lib/features/user/models/user_response.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserResponse _$UserResponseFromJson(Map<String, dynamic> json) => UserResponse(
+      id: (json['id'] as num).toInt(),
+      username: json['username'] as String,
+      phoneNumber: json['phoneNumber'] as String,
+    );
+
+Map<String, dynamic> _$UserResponseToJson(UserResponse instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'username': instance.username,
+      'phoneNumber': instance.phoneNumber,
+    };
