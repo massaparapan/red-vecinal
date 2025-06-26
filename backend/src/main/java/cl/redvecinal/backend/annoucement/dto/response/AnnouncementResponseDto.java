1469a7216bddb73310c652b7e2515e8f4d7b9839
iiesteban <e.ortiz06@ufromail.cl>
2025-06-25 23:28:23 -0400

    feat(announcement): add createdAt field to AnnouncementResponseDto

--- backend/src/main/java/cl/redvecinal/backend/annoucement/dto/response/AnnouncementResponseDto.java
+++ backend/src/main/java/cl/redvecinal/backend/annoucement/dto/response/AnnouncementResponseDto.java
@@ -4,11 +4,14 @@ import cl.redvecinal.backend.annoucement.model.AnnouncementType;
 import cl.redvecinal.backend.user.dto.request.UserDto;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class AnnouncementResponseDto {
     private Long id;
     private String title;
     private String content;
+    private LocalDateTime createdAt;
     private AnnouncementType type;
     private UserDto createdBy;
 }
